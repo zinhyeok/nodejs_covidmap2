298fc7bb38b1aaa6b6304459e22956558068bee5
zinhyeok <david9pjh@gmail.com>
2022-03-12 16:41:58 +0900

    [Fetch: 마커 표시 기능, 기본 마커에서 디자인 변경]

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,4 +3,32 @@ var mapOptions = {
     zoom: 10,
   };
 
-  var map = new naver.maps.Map("map", mapOptions);
+var map = new naver.maps.Map("map", mapOptions);
+
+const data = [
+  {
+    title: "용산역",
+    address: "용산",
+    lat:37.52995115069976, 
+    lng:126.96479627575842,
+  },
+
+  {
+    title: "서울역",
+    address: "서울",
+    lat:37.55619031742226, 
+    lng: 126.9724214141688,
+  }
+]
+
+for (let i in data){
+  const target = data[i];
+  const latlng = new naver.maps.LatLng(target.lat, target.lng);
+  let marker = new naver.maps.Marker({
+    map: map,
+    position: latlng,
+    icon: 
+    {content : `<div class="marker"></div>`,
+    },
+  });
+}
