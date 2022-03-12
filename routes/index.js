f179b811654c821d9fe2ded095f88457a12596a4
zinhyeok <david9pjh@gmail.com>
2022-03-12 16:20:16 +0900

    [Fetch: uploadpage 연결]

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ router.get('/', function(req, res, next) {
   console.log("start")
   res.render('index', { title: 'Express' });
 });
+/* GET upload page. */
+router.get('/upload', (req, res, next) => {
+  console.log("upload")
+  res.render('upload', { title: 'Express' });
+});
 
 module.exports = router;
