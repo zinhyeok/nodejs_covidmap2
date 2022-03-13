2b6ef132a312eb4ddc920f225f6eb8f8eed96911
zinhyeok <david9pjh@gmail.com>
2022-03-13 10:31:47 +0900

    [Fetch: 마커 클릭시 inforwindow뜸]

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -23,9 +23,22 @@ const data = [
 
 let markerList = [];
 let infowindowList = [];
+
+const getClickHandler = (i) => () => {
+  const marker = markerList[i];
+  const inforwindow = infowindowList[i];
+  if(inforwindow.getMap()){
+    inforwindow.close();
+  }
+  else{
+    inforwindow.open(map, marker);
+  }
+};
+
 for (let i in data){
   const target = data[i];
   const latlng = new naver.maps.LatLng(target.lat, target.lng);
+
   let marker = new naver.maps.Marker({
     map: map,
     position: latlng,
@@ -34,20 +47,24 @@ for (let i in data){
     acnhor: new naver.maps.Point(7.5,7.5),
     },
   });
+
+
+  const content = `
+  <div class="infowindow_wrap">
+    <div class="infowindow_title">${target.title}</div>
+    <div class="infowindow_address">${target.address}</div>
+  </div>`;
+
+  const infowindow = new naver.maps.InfoWindow({
+    content: content,
+    backgroundColor: "#00ff0000",
+    borderColor: "#00ff0000",
+    anchorSize: new naver.maps.Size(0,0)
+});
+    markerList.push(marker);
+    infowindowList.push(infowindow);
 }
 
-const content = `
-<div class="infowindow_wrap">
-  <div class="infowindow_title">${target.title}</div>
-  <div class="infowindow_address">${target.address}</div>
-</div>`;
-
-const infowindow = new naver.maps.Infowindow({
-  content: content,
-  backgroundColor:"#00ff0000",
-  borderColor:"#00ff0000",
-  anchorSizw: new naver.maps.Size(0,0),
-});
-
-markerList.push(marker);
-infowindowList.push(infowindow);
+for (let i=0, ii = markerList.length; i<ii; i++){
+  naver.maps.Event.addListener(markerList[i],"click",getClickHandler(i));
+}
