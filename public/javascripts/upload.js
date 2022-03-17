e737b216a644f25123e0f1b2728bbcdeae6c2e25
zinhyeok <david9pjh@gmail.com>
2022-03-17 21:32:26 +0900

    Revert "[Fetch: 키워드 검색결과 받아오기_console출력]"
    This reverts commit 50a65a7707e4f79bb6cf1d0d851e410c2f692174.

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -6,34 +6,3 @@ const mapOption = {
 };
 
 let map = new daum.maps.Map(mapContainer, mapOption);
-
-let infowindow = new daum.maps.InfoWindow({
-    zIndex: 1,
-});
-
-let markerList = [];
-
-let ps = new daum.maps.services.Places();
-
-searchPlaces();
-
-function searchPlaces(){
-    let keyword = $("#keyword").val();
-    ps.keywordSearch(keyword, placesSearchCB);
-}
-
-function placesSearchCB(data, status){
-    if (status === daum.maps.services.Status.OK){
-        displayPlaces(data);
-    }else if (status === daum.maps.services.Status.ZERO_RESULT){
-        alert("검색 결과가 존재하지 않습니다");
-        return;
-    }else if(status === daum.maps.services.Status.ERROR){
-        alert("검색 중 오류가 발생했습니다");
-        return;
-    }
-}
-
-function displayPlaces(data){
-    
-}
