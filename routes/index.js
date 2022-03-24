14e7b4b972a0ce28c438990a8b253d042c0fb836
zinhyeok <david9pjh@gmail.com>
2022-03-24 17:41:42 +0900

    Feat: [postman 설치+비구조화 할당]

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,22 @@ router.get('/', function(req, res, next) {
 });
 /* GET upload page. */
 router.get('/upload', (req, res, next) => {
-  console.log("upload")
-  res.render('upload', { title: 'Express' });
+  console.log("upload");
 });
 
+router.get("/test", (req,res,next)=>{
+  console.log("test 완료");
+  res.json({
+    message: "resonse 완료",
+  });
+});
+
+router.post("/test", (req,res,next)=>{
+  const {test, test2} = req.body;
+  console.log(test);
+  console.log(test2);
+  res.json({
+    message: "post response 완료",
+  });
+});
 module.exports = router;
