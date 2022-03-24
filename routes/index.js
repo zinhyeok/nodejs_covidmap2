9104e7d2afefc85c969686717d1eea4bd0034c08
zinhyeok <david9pjh@gmail.com>
2022-03-24 17:53:29 +0900

    Feat: [location 서버에 저장]

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const locationModel = require("../model/location");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,19 +12,24 @@ router.get('/upload', (req, res, next) => {
   console.log("upload");
 });
 
-router.get("/test", (req,res,next)=>{
-  console.log("test 완료");
-  res.json({
-    message: "resonse 완료",
+router.post("/location", (req,res,next)=>{
+  const {title, address, lat, lng} = req.body;
+  let location = new locationModel();
+  location.title = title;
+  location.address = address;
+  location.lat = lat;
+  location.lng = lng;
+  location.save().then(result => {
+    console.log(result)
+    res.json({
+      message: "post success",
+    });
+  }).catch((error)=>{
+    console.log(error);
+    res.json({
+      message: "post error",
+    });
   });
-});
 
-router.post("/test", (req,res,next)=>{
-  const {test, test2} = req.body;
-  console.log(test);
-  console.log(test2);
-  res.json({
-    message: "post response 완료",
-  });
 });
 module.exports = router;
